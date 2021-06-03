ec15890f0b75de7adf3dcdfdafbce159e8214945
iambo <61999274+iambo17@users.noreply.github.com>
2021-06-03 23:20:01 +0800

    Delete src/main/java/iambo directory

--- src/main/java/iambo/util/MoneyUtil.java
+++
@@ -1,6 +0,0 @@
-package iambo.util;
-
-public class MoneyUtil {
-    public String player1_money_file="C:\\Users\\20400\\IdeaProjects\\Monopoly\\src\\main\\java\\iambo\\inf\\player1_money.txt";
-    public String player2_money_file="C:\\Users\\20400\\IdeaProjects\\Monopoly\\src\\main\\java\\iambo\\inf\\player2_money.txt";
-}
